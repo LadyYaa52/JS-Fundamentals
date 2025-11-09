b98b9b12a900ceaee75bb5bb0edd77ce39aba06c
Yaa Boatemaa Acheampong <yhahboatemaa52@gmail.com>
2025-11-09 09:03:46 -0800

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,12 +1,13 @@
 const args = process.argv;
 const process = require('node:process');
-if (args.length === 0) {
+if (process.argv.length === 2) {
   console.log("No argument");
-} else if (args.length === 1) {
+} else if (process.argv.length === 3) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
 }
+
 
 
 
