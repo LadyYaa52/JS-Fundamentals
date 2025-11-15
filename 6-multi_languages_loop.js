f9a185bc69139542846461813ede339227db687a
Yaa Boatemaa Acheampong <yhahboatemaa52@gmail.com>
2025-11-15 15:02:10 -0800

    Update 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Define the array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Use a loop to go through the array
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
