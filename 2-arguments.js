3e9ead2094e2145f6c7d3b6382f7c5d823f88598
Yaa Boatemaa Acheampong <yhahboatemaa52@gmail.com>
2025-11-15 15:49:50 -0800

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,17 +1,3 @@
-#!/usr/bin/node
-
-// Count arguments excluding "node" and script name
-const argsCount = process.argv.slice(2).length;
-
-if (argsCount === 0) {
-  console.log('No argument');
-} else if (argsCount === 1) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
-
-
 
 
 
